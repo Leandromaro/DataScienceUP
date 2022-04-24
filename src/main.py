09df54b4fd817e6c80abbaa56704623d7aa65293
Lucas <lhbelfanti@gmail.com>
2022-04-24 17:29:06 -0300

    Added time measurement.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+from timeit import default_timer as timer
 from clean import clean_data
 from group import group_data
 
 if __name__ == '__main__':
+	start = timer()
 	print("--- Cleaning Data ---")
-	#clean_data()
+	clean_data()
+	end_clean_data = timer()
+	print("Took: " + str(end_clean_data - start) + " seconds")
 	print("-- Grouping Data ---")
 	group_data()
+	end_group_data = timer()
+	print("Took: " + str(end_group_data - end_clean_data) + " seconds")
 
