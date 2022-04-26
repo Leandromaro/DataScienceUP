d586e2731f6b1da40ffcb656c820ee0b95824521
Leandro Maro <leandromaro@BTSs-MacBook-Pro-2.local>
2022-04-25 22:18:03 -0300

    Adding pot chart

--- src/chart/chart.py
+++ src/chart/chart.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import os
+
+import numpy as np
 import pandas as pd
 
 dirname = os.path.dirname(__file__)
 COUNTRIES_PERCENTAGE_GROUP_PATH = os.path.join(dirname, '../res/group/countries_percentage.csv')
+CITY_TEMPERATURE_GROUP_ARGENTINA_BY_YEAR_PATH = os.path.join(dirname, '../res/group/argentina_temperature_by_year.csv')
 
 
 def pie_chart():
@@ -19,4 +22,19 @@ def pie_chart():
     plt.show()
 
 
-pie_chart()
+def pie_plot():
+    df = pd.read_csv(CITY_TEMPERATURE_GROUP_ARGENTINA_BY_YEAR_PATH, low_memory=False)
+    x = df.Year
+
+    y = df.YearAvgTemperature
+
+    fig = plt.figure(figsize=(12, 6))
+    plt.xlabel("Years")
+    plt.ylabel("Temperature Average")
+    ax = fig.add_subplot(111)
+    ax.bar(np.arange(len(x)), y)
+    ax.set_xticks(np.arange(len(x)))
+    ax.set_xticklabels(x, rotation=15, zorder=100)
+
+    plt.savefig('argentina_plot.png')
+    plt.show()
